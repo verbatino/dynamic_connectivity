655c99b8e6386c020a37c56c5f64c6c5e672e6bb
student <student@10.2.5.6>
2014-11-22 12:12:40 +0100

    commit

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,19 +3,26 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
-	
+
 	public UF(int N) {
 		id = new int[N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
 	}
-	
-	public void union(int p, int q) {
-	}
-	
+
 	public boolean connected(int p, int q) {
-		return false;
+		return id[p] == id[q];
 	}
-	
+
+	public void union(int p, int q) {
+		int pid = id[p];
+		int qid = id[q];
+		for (int i = 0; i < id.length; ++i) {
+			if (id[i] == pid) {
+				id[i] = qid;
+			}
+		}
+	}
+
 }
