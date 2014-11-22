2186222acf042989be83897cee8f228195192964
student <student@10.2.5.6>
2014-11-22 12:39:03 +0100

    g

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -16,7 +16,7 @@ public class UF {
 	}
 
 	public void union(int p, int q) {
-		int pid = id[p];
+		int pid = id[p]; 
 		int qid = id[q];
 		for (int i = 0; i < id.length; ++i) {
 			if (id[i] == pid) {
