83e7cec3d175a0a4bbba223d664e21edab38c2b6
verbatino <dev.verbatino@gmail.com>
2014-12-06 12:36:06 +0100

    Swiezy z funkcjami

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -9,20 +9,21 @@ public class UF {
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
+	}	
+	private int root(int i) {
+		  while (i != id[i]) {
+		    i = id[i];
+		  }
+		  return i;
 	}
-
 	public boolean connected(int p, int q) {
-		return id[p] == id[q];
+		  return root(p) == root(q);
 	}
-
 	public void union(int p, int q) {
-		int pid = id[p]; 
-		int qid = id[q];
-		for (int i = 0; i < id.length; ++i) {
-			if (id[i] == pid) {
-				id[i] = qid;
-			}
-		}
+		  int i = root(p);
+		  int j = root(q);
+		  id[i] = j;
 	}
+	
 
 }
